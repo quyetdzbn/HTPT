ee4a6426ebafbce76a5289250da36df3f7cc058b
duonglh <duonglh10@gmail.com>
2023-11-13 15:42:13 +0700

    duonglh update 13/11

--- src/main/java/com/example/demo/controller/CustomerController.java
+++ src/main/java/com/example/demo/controller/CustomerController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@Controller
+@RestController
 public class CustomerController {
     @Autowired
     CustomerService service;
--- src/main/java/com/example/demo/controller/FoodController.java
+++ src/main/java/com/example/demo/controller/FoodController.java
@@ -3,16 +3,25 @@ package com.example.demo.controller;
 import com.example.demo.entity.Food;
 import com.example.demo.service.FoodService;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
 
 @Controller
+@RequestMapping("/food")
 public class FoodController {
     private FoodService service;
 
     public FoodController(FoodService service){
         this.service = service;
+    }
+
+    @GetMapping
+    public String getAll(Model model){
+        List<Food> foodList = service.getAll();
+        model.addAttribute("foodList", foodList);
+        return "food";
     }
 }
