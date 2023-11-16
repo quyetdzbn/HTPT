cf3d458c945cebf9c0345d610ecbe4e7fd59d6b8
duonglh <duonglh10@gmail.com>
2023-11-16 22:06:06 +0700

    duonglh update 16/11

--- src/main/java/com/example/demo/controller/FoodController.java
+++ src/main/java/com/example/demo/controller/FoodController.java
@@ -2,15 +2,16 @@ package com.example.demo.controller;
 
 import com.example.demo.entity.Food;
 import com.example.demo.service.FoodService;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.security.Principal;
 import java.util.List;
 
 @Controller
-@RequestMapping("/food")
 public class FoodController {
     private FoodService service;
 
@@ -18,10 +19,64 @@ public class FoodController {
         this.service = service;
     }
 
-    @GetMapping
-    public String getAll(Model model){
-        List<Food> foodList = service.getAll();
-        model.addAttribute("foodList", foodList);
-        return "food";
+    @GetMapping("/foods")
+    public String foods(Model model, Principal principal){
+//        if(principal == null){
+//            return "redirect:/login";
+//        }
+        List<Food> foods = service.getAll();
+        model.addAttribute("foodList", foods);
+        model.addAttribute("size", foods.size());
+        model.addAttribute("title", "Food");
+        model.addAttribute("foodNew", new Food());
+        return "foods";
+    }
+
+    @PostMapping("/add-food")
+    public String add(@ModelAttribute("foodNew") Food food, RedirectAttributes attributes){
+        try {
+            service.save(food);
+            attributes.addFlashAttribute("success", "Added successfully!");
+        }catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Failed to add because duplicate name!");
+        }catch (Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Error server!");
+        }
+        return "redirect:/foods";
+    }
+
+    @RequestMapping(value = "/findById", method = {RequestMethod.PUT, RequestMethod.GET})
+    @ResponseBody
+    public Food getById(Integer id){
+        return service.getById(id);
+    }
+
+    @GetMapping("/update-food")
+    public String update(Food food, RedirectAttributes attributes){
+        try {
+            service.save(food);
+            attributes.addFlashAttribute("success", "Updated successfully!");
+        }catch (DataIntegrityViolationException e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Failed to update because duplicate name");
+        }catch (Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Error server");
+        }
+        return "redirect:/foods";
+    }
+
+    @RequestMapping(value = "/delete-food", method = {RequestMethod.PUT, RequestMethod.GET})
+    public String delete(Integer id, RedirectAttributes attributes){
+        try {
+            service.delete(id);
+            attributes.addFlashAttribute("success", "Deleted successfully");
+        }catch (Exception e){
+            e.printStackTrace();
+            attributes.addFlashAttribute("failed", "Failed to deleted");
+        }
+        return "redirect:/foods";
     }
 }
--- src/main/java/com/example/demo/repo/FoodRepo.java
+++ src/main/java/com/example/demo/repo/FoodRepo.java
@@ -2,6 +2,11 @@ package com.example.demo.repo;
 
 import com.example.demo.entity.Food;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
 
 public interface FoodRepo extends JpaRepository<Food, Integer> {
+    @Query("select f from Food f where f.name_foo like %?1% or f.supplier like %?1%")
+    List<Food> searchFood(String keyword);
 }
